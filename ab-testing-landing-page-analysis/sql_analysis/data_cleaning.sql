f75e55df92d67ad215a55334bb6a2d57edb35e8c
Md Faizan Ali <faizanmba2022@gmail.com>
2026-05-24 20:21:02 +0530

    Delete ab-testing-landing-page-analysis directory

--- ab-testing-landing-page-analysis/sql_analysis/data_cleaning.sql
+++
@@ -1,8 +0,0 @@
--- =========================================
--- DATA CLEANING
--- =========================================
-
--- Remove Blank Experiment Groups
-DELETE FROM ab_data
-WHERE test_group IS NULL
-   OR test_group = '';
